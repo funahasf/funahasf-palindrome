264631a247e42e711ac22e39a267d95538cdda29
funahasf <funahasf@gmail.com>
2022-04-13 12:46:23 +0900

    Finish working and refactored palindrome method

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
+module.exports = Phrase;
+
 // Adds `reverse` to all strings.
 String.prototype.reverse = function() {
   return Array.from(this).reverse().join("");
 }
 
-
 // Defines a Phrase object.
 function Phrase(content) {
-  this.content = content; 
+  this.content = content;
 
-  // Returns true for a palindrome, false otherwise.
+  // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
-    return this.content.toLowerCase();
-}
+    return this.letters().toLowerCase();
+  }
+
+  // Returns the letters in the content.
+  // For example:
+  //   new Phrase("Hello, world!").letters() === "Helloworld"
+  this.letters = function letters() {
+    return (this.content.match(/[a-z]/gi || [])).join("");
+  }
 
   // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
